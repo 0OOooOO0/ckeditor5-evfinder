5fa6a2a012966cc522b8b75726d841c63c600a3d
NC <NC@gruenag.local>
2017-11-14 13:39:55 +0100

    Add multiple images support

--- src/evfinderaddimagecommand.js
+++ src/evfinderaddimagecommand.js
@@ -1,4 +1,3 @@
-
 import ModelElement from '@ckeditor/ckeditor5-engine/src/model/element';
 import ModelRange from '@ckeditor/ckeditor5-engine/src/model/range';
 import ModelSelection from '@ckeditor/ckeditor5-engine/src/model/selection';
@@ -14,24 +13,31 @@ export default class EVFinderAddImageCommand extends Command {
 		const selection = doc.selection;
 
 		doc.enqueueChanges( () => {
-			const imageElement = new ModelElement( 'image', {
-				src: url
-			} );
+			url.forEach((urlelem, idx) => {
+				const imageElement = new ModelElement( 'image', {
+					src: urlelem
+				} );
 
-			let insertAtSelection;
+				let insertAtSelection;
 
-			if ( options.insertAt ) {
-				insertAtSelection = new ModelSelection( [ new ModelRange( options.insertAt ) ] );
-			} else {
-				insertAtSelection = doc.selection;
-			}
+				if ( options.insertAt ) {
+					insertAtSelection = new ModelSelection( [ new ModelRange( options.insertAt ) ] );
+				} else {
+					insertAtSelection = doc.selection;
+				}
 
-			editor.data.insertContent( imageElement, insertAtSelection, batch );
+				// need to get the next position, otherwise will replace the last image
+				if (insertAtSelection._ranges.length != 0) {
+					insertAtSelection._ranges[0].start.path[0] += idx;
+				}
 
-			// Inserting an image might've failed due to schema regulations.
-			if ( imageElement.parent ) {
-				selection.setRanges( [ ModelRange.createOn( imageElement ) ] );
-			}
-		} );
+				editor.data.insertContent( imageElement, insertAtSelection, batch );
+
+				// Inserting an image might've failed due to schema regulations.
+				if ( imageElement.parent ) {
+					selection.setRanges( [ ModelRange.createOn( imageElement ) ] );
+				}
+			});
+		});
 	}
 }
